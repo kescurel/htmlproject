b42dc265867144b5987cc62f696fa4410ad5faf8
kescurel <51978685+kescurel@users.noreply.github.com>
2019-07-06 01:11:13 +0800

    Add files via upload

---
+++ kev-project/scripts/script.js
@@ -0,0 +1,10 @@
+// for testing
+console.log("JS Script Connected");
+
+$('#button-proceed').click(function() {
+    swal('Finalized Flight Booking Details', 'Congratulations, please proceed to next step', 'success')
+});
+
+
+
+
